8a6ad43a1e93d9713b89a02126f418bed091ee7b
dharmik8488 <118181437+dharmik8488@users.noreply.github.com>
2022-11-17 07:31:35 -0800

    Add files via upload

---
+++ dharmik day4.5.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+ void main(){
+ 
+     int c=40;
+	 int d=30;   
+     
+     if(c>d){
+     	
+     	 printf("C is minimum");
+     	
+     	
+     	
+	 }
+ 
+         else if (c<d){
+         	
+         	printf("D is minimum");  		 }
+        
+ 
+ 
+ 
+ 
+ 
+ 
+ }
---
+++ dharmik4.6.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+
+ void main(){
+ 	
+ 	 int d=0;
+ 	 
+ 	  printf("enter the number d:");
+ 	  
+ 	     scanf("%d",&d);
+ 	     
+ 	 if(d>0){
+ 	 	
+ 	 	printf("yas positive number");
+ 	 	
+	  } 
+	  
+	  else if(d<0){
+	  	
+	  	      printf("negative number");
+	  	      
+	  }   
+	  
+	  else if(d==0){
+	  	
+	  	      printf("no positive negative number");
+	  }
+ 	
+ 	
+ 	
+ 	
+ 	
+ }
